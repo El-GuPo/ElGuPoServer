95f30c49c43e56a172295273323d200b34d53e03
apdRV <apodmarev@list.ru>
2025-02-17 04:10:05 +0300

    initial-commit

---
+++ src/main/java/com/elgupo/elguposerver/ElGuPoServerApplication.java
@@ -0,0 +1,13 @@
+package com.elgupo.elguposerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElGuPoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElGuPoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elgupo/elguposerver/ElGuPoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elgupo.elguposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElGuPoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
