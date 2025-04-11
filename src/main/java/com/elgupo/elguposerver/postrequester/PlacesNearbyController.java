345a5c13d6e9d1788216ec20d1a90ec9e8540c8f
mrnchk <murlinmiron@gmail.com>
2025-04-11 03:19:36 +0300

    controller

---
+++ src/main/java/com/elgupo/elguposerver/postrequester/PlacesNearbyController.java
@@ -0,0 +1,23 @@
+package com.elgupo.elguposerver.postrequester;
+
+import com.elgupo.elguposerver.dataclasses.Place;
+import com.elgupo.elguposerver.postrequester.PostRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PlacesNearbyController {
+    @GetMapping("/places-nearby")
+    public List<Place> getPlaces(
+            @RequestParam Double latitude,
+            @RequestParam Double longitude,
+            @RequestParam int count,
+            @RequestParam Double radius
+    ) throws IOException, InterruptedException {
+        return PostRequester.getPlacesNearby(latitude, longitude, count, radius);
+    }
+}
