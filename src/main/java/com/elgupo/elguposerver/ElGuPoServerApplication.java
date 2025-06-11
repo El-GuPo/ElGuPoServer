a992d75ecab81008ad22b2a14558a1a32cba4388
apdRV <apodmarev@list.ru>
2025-06-11 19:21:34 +0300

    fix: return likesCleaner constructor in main

--- src/main/java/com/elgupo/elguposerver/ElGuPoServerApplication.java
+++ src/main/java/com/elgupo/elguposerver/ElGuPoServerApplication.java
@@ -1,5 +1,6 @@
 package com.elgupo.elguposerver;
 
+import com.elgupo.elguposerver.database.LikesCleaner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.elgupo.elguposerver.postrequester.*;
@@ -9,6 +10,7 @@ public class ElGuPoServerApplication {
 
     public static void main(String[] args) {
         ActualEventsHolder.getInstance();
+        new LikesCleaner();
         SpringApplication.run(ElGuPoServerApplication.class, args);
     }
 
