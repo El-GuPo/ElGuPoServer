517b4e51f42b9705c15a477eaab340c24dbd9c21
mrnchk <143181833+mrnchk@users.noreply.github.com>
2025-06-12 03:36:28 +0300

    reset

--- src/main/java/com/elgupo/elguposerver/dataclasses/Category.java
+++ src/main/java/com/elgupo/elguposerver/dataclasses/Category.java
@@ -22,7 +22,7 @@ public class Category {
     public static Category THEATER = new Category("Театр", 7);
     public static Category NOCATEGORY = new Category("", -1);
 
-    public static Category[] CATEGORIES = {CINEMA, SPORT, EXHIBITION, CONCERTS, THEATER, NOCATEGORY};
+    public static Category[] CATEGORIES = {CINEMA, SPORT, EXHIBITION, CONCERTS, THEATER};
 
     public static Category getCategoryById(int id) {
         for (Category category : CATEGORIES) {
